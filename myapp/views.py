dd1eaf237542278db531d3f320bb18294212f7d2
vandanaps1992 <vandanaps1992@gmail.com>
2022-03-11 12:32:06 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return HttpResponse('welcome to myapp')
+
+
+def home(req):
+    return render(req, 'home.html')
