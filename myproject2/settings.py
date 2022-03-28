7f2a29ad3f495c8d7e2194fc3321e07113fee970
vandanaps1992 <vandanaps1992@gmail.com>
2022-03-28 15:58:56 +0530

    new

--- myproject2/settings.py
+++ myproject2/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.Whitenoisemiddleware',
+    'whitenoise.middleware.Whitenoisemiddleware',
 ]
 
 ROOT_URLCONF = 'myproject2.urls'
